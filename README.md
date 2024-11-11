bd4843c941c304f35be443cb70a5171ef5d61d41
Rich Brown <richb.hanover@gmail.com>
2024-11-11 09:50:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Check My Markdown
+
+A script to check the formatting of Markdown files.
+
+**Usage:**
+
+```bash
+sh ./check-my-markdown.sh path-to-markdown-files
+```
+
+This runs the following tools:
+
+* [markdownlint](https://github.com/DavidAnson/markdownlint)
+* [markdown-link-check](https://github.com/tcort/markdown-link-check)
+* [spellchecker-cli](https://github.com/tbroadley/spellchecker-cli)
---
+++ check-my-markdown.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# check-my-markdown.sh
+# Run tools on a set of Markdown files to find fussy problems
+# Decide whether to fix or not
+
+# Usage: 
+# 
+#   sh ./check-my-markdown.sh path-to-top-level-directory
+
+# Use "find" to recursively discover all the *.md files and pipe
+# them to the tools
+
+DIR=`pwd`
+DICT="$DIR/dictionary.txt"
+# echo ' --- "$DICT" --- '
+
+echo ""
+echo "=== markdownlint ==="
+echo ""
+find "$1" -name \*.md -print0 | xargs -0 -n1 markdownlint
+
+echo ""
+echo "=== markdown-link-check ==="
+echo ""
+find "$1" -name \*.md -print0 | xargs -0 -n1 markdown-link-check
+
+echo ""
+echo "=== spellchecker ==="
+echo ""
+
+cd "$1"
+spellchecker -q --files "**/*.md" -d "$DICT"
